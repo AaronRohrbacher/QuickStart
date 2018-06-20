1069b9997320d7d6ed1fdf546052bd880fc27b6c
Aaron Rohrbacher <arjazz@icloud.com>
2018-06-19 22:21:32 -0700

    customize devise views, add custom params to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :business_name, :address_1, :address_2, :city, :state, :zip, :phone_1, :phone2])
+  end
 end
