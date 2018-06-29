36c61f41f38fb2361790f11d4318aa8232a81ea9
Aaron Rohrbacher <arjazz@icloud.com>
2018-06-28 21:05:20 -0700

    add editing and deletion of line items, css styling to keep links off pdf

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -11,6 +11,27 @@ class LineItemsController < ApplicationController
     redirect_to new_invoice_line_item_path(@invoice)
   end
 
+  def edit
+    @invoice = Invoice.find(params[:invoice_id])
+    @line_item = LineItem.find(params[:id])
+  end
+
+  def update
+    @invoice = Invoice.find(params[:invoice_id])
+    @line_item = LineItem.find(params[:id])
+    @line_item.update!(line_item_params)
+    @invoice.update_invoice_total
+    redirect_to invoice_path(@invoice)
+  end
+
+  def destroy
+    @line_item = LineItem.find(params[:id])
+    @invoice = Invoice.find(params[:invoice_id])
+    @line_item.destroy!
+    @invoice.update_invoice_total
+    redirect_to invoice_path(@invoice)
+  end
+
   private
   def line_item_params
     params.require(:line_item).permit(:invoice_id, :description, :quantity, :price, :notes)
