5a3e16fe443e3199d063d0f225cb5ff1dc0dc8a0
Aaron Rohrbacher <arjazz@icloud.com>
2018-06-30 00:10:59 -0700

    allow deletion of invoices

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -37,6 +37,12 @@ class InvoicesController < ApplicationController
     redirect_to invoice_path(@invoice)
   end
 
+  def destroy
+    @invoice = Invoice.find(params[:id])
+    @invoice.destroy
+    redirect_to root_path
+  end
+
   private
   def invoice_params
     params.require(:invoice).permit(:company_id, :user_id, :invoice_number, :sent_date, :due_date, :paid)
